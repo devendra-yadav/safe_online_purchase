4217e29dcd139b63123135076f7093abc84ddded
Devendra Yadav <devendra.yadav@gmail.com>
2022-08-26 02:15:46 +0800

    completed all basic functionality

--- contracts/OnlinePurchaseAgreement.sol
+++ contracts/OnlinePurchaseAgreement.sol
@@ -14,40 +14,60 @@ contract OnlinePurchaseAgreement {
         CREATED,
         LOCKED,
         ITEM_RECEIVED,
-        PAYMENT_RECEIVED,
         COMPLETED,
+        INACTIVE,
         FAILED
     }
 
     PurchaseStatus public purchaseStatus;
 
-    ///This function cant be called in current state
-    error inValidState();
-
-    event ContractLocked(address buyer, address seller, uint256 purchageAmount);
+    event ContractLocked(address indexed buyer, address indexed seller, uint256 purchageAmount, PurchaseStatus status);
+    event ItemReceived(address indexed buyer, address indexed seller, uint256 purchageAmount, PurchaseStatus status);
+    event ContractCompleted(address indexed buyer, address indexed seller, uint256 purchageAmount, PurchaseStatus status);
+    event ContractAborted(address indexed buyer, PurchaseStatus status);
 
     modifier inState(PurchaseStatus _status) {
-        require(purchaseStatus == _status, "This function cant be called at current purchase status");
+        require(purchaseStatus == _status,"Cant run this function in current state." );
         _;
     }
 
     constructor(uint256 _value) payable {
         require(_value == msg.value, "Please send correct amount. Same as purchase amount");
-        //purchaseStatus=PurchaseStatus.CREATED;
+       
         value = _value;
-        purchaseStatus=PurchaseStatus.CREATED;
-        console.log("AAAAAAA",purchaseStatus);
+        
         seller = payable(msg.sender);
     }
 
-    function confirmPurchase() public payable inState(PurchaseStatus.CREATED){
-
+    function confirmPurchase() external payable inState(PurchaseStatus.CREATED){
+        
         require(msg.value == 2*value, "Please send twice the purchase amount");
 
         buyer = payable(msg.sender);
         purchaseStatus = PurchaseStatus.LOCKED;
-
-        emit ContractLocked(buyer, seller, value);
         
+        emit ContractLocked(buyer, seller, value, purchaseStatus);
+        
+    }
+
+    function confirmReceived() external inState(PurchaseStatus.LOCKED) {
+        require(msg.sender==buyer,"Only buyer can call this method");
+        purchaseStatus = PurchaseStatus.ITEM_RECEIVED;
+        buyer.transfer(value);
+        emit ItemReceived(buyer, seller, value, purchaseStatus);
+    }
+
+    function paySeller() external inState(PurchaseStatus.ITEM_RECEIVED){
+        require(msg.sender==seller,"Only seller can call this method");
+        purchaseStatus = PurchaseStatus.COMPLETED;
+        seller.transfer(2*value);
+        emit ContractCompleted(buyer, seller, value, purchaseStatus);
+    }
+
+    function abort() external inState(PurchaseStatus.CREATED){
+        require(msg.sender==seller,"Onlyseller can call this method");
+        purchaseStatus = PurchaseStatus.INACTIVE;
+        seller.transfer(value);
+        emit ContractAborted(buyer, purchaseStatus);
     }
 }
--- test/onlinePurchaseAgreementTest.js
+++ test/onlinePurchaseAgreementTest.js
@@ -1,7 +1,7 @@
 
 const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers")
-const {ethers} = require('hardhat');
-const {expect} = require("chai");
+const {ethers} = require("hardhat");
+const {expect, assert} = require("chai");
 
 let formatEther = (n)=>{
     return ethers.utils.parseEther(n.toString());
@@ -13,21 +13,23 @@ let humanReadableAmount = (n)=>{
 
 describe("OnlinePurchaseAgreement", ()=>{
     async function contractDeployment(){
-        const {deployer, buyer} = await ethers.getSigners();
+        let accounts = await ethers.getSigners();
+        let deployer = accounts[0];
+        let buyer = accounts[1];
 
         const OnlinePurchaseAgreement = await ethers.getContractFactory("OnlinePurchaseAgreement");
         let value=1000;
         const onlinePurchaseAgreement = await OnlinePurchaseAgreement.deploy(formatEther(value), {value: formatEther(value)});
         await onlinePurchaseAgreement.deployed();
         
-        return {onlinePurchaseAgreement, buyer }
+        return {onlinePurchaseAgreement, deployer, buyer, value}
     }
 
     describe("Contract Deployment", ()=>{
         it("should deploy the contract successfully", async ()=>{
             const {onlinePurchaseAgreement} = await loadFixture(contractDeployment);
             console.log(`OnlinePurchageAgreement deployed at ${onlinePurchaseAgreement.address}`)
-            //console.log(`agreement status : ${onlinePurchaseAgreement.purchaseStatus}`)
+           
             expect(onlinePurchaseAgreement.address).to.be.properAddress;
            // expect(onlinePurchaseAgreement.purchaseStatus).to.be.equal(0);
         })
@@ -47,10 +49,62 @@ describe("OnlinePurchaseAgreement", ()=>{
 
     describe("Contract functionality", ()=>{
         it("buyer should be able to lock the agreement to buy", async ()=>{
-            const {onlinePurchaseAgreement, buyer} = await loadFixture(contractDeployment);
+            const {onlinePurchaseAgreement, buyer, value} = await loadFixture(contractDeployment);
             
-            await onlinePurchaseAgreement.connect(buyer).confirmPurchase();
+            await onlinePurchaseAgreement.connect(buyer).confirmPurchase({value: formatEther(2*value)});
 
+            let contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance : ${contractBalance}`);
+            expect(contractBalance).to.be.equal('3000.0');    
+        })
+
+        it("buyer should be able to confirm the receipt of the items", async ()=>{
+            const {onlinePurchaseAgreement, buyer, value} = await loadFixture(contractDeployment);
+            await onlinePurchaseAgreement.connect(buyer).confirmPurchase({value: formatEther(2*value)});
+
+            await onlinePurchaseAgreement.connect(buyer).confirmReceived();
+            let contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance : ${contractBalance}`);
+            expect(contractBalance).to.be.equal('2000.0');
+        })
+
+        it("seller should be able to receive the amount after buyer received the items", async ()=>{
+            const {onlinePurchaseAgreement,deployer, buyer, value} = await loadFixture(contractDeployment);
+            await onlinePurchaseAgreement.connect(buyer).confirmPurchase({value: formatEther(2*value)});
+
+            await onlinePurchaseAgreement.connect(buyer).confirmReceived();
+
+            await onlinePurchaseAgreement.connect(deployer).paySeller();
+
+            let contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance : ${contractBalance}`);
+            expect(contractBalance).to.be.equal('0.0');
+        })
+
+        it("seller should be able to abort the contract if buyer didnt pay yet", async ()=>{
+            const {onlinePurchaseAgreement,deployer, buyer, value} = await loadFixture(contractDeployment);
+            let contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance Before : ${contractBalance}`);
+
+            await onlinePurchaseAgreement.connect(deployer).abort();
+
+            contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance After : ${contractBalance}`);
+            expect(contractBalance).to.be.equal('0.0');
+
+        })
+
+        it("seller should not be able to abort the contract if buyer alreay paid", async ()=>{
+            const {onlinePurchaseAgreement,deployer, buyer, value} = await loadFixture(contractDeployment);
+            let contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance Before : ${contractBalance}`);
+            await onlinePurchaseAgreement.connect(buyer).confirmPurchase({value: formatEther(2*value)});
+            
+            await expect(onlinePurchaseAgreement.connect(deployer).abort()).to.be.reverted;
+
+            contractBalance = humanReadableAmount(await ethers.provider.getBalance(onlinePurchaseAgreement.address));
+            console.log(`Contract balance After : ${contractBalance}`);
+            expect(contractBalance).to.be.equal('3000.0');
 
         })
     })
